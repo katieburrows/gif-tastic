ff994425179e0ded7e900a1e04427e2ad977ca12
katieburrows <katie_burrows@me.com>
2019-04-17 16:41:52 -0600

    added state to file, got gifs to start and stop based off of click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$("#buttonDisplay").empty();
 	
 	//Original array of topics
-	var topics = ["puppy", "kitten", "dog", "cat", "owl", "squirrel", "lady bug", "bird", "falcon", "fish", "bear", "monkey", "zebra", "lion", "elephant"];
+	var topics = ["puppy", "kitten", "dog", "cat", "owl", "squirrel", "lady bug", "bird", "falcon", "fish", "bear", "monkey", "zebra", "lion", "elephant", "antelope", "deer", "crow"];
 
 	//Function with for loop that goes through each topic in the array and creates a button, adds a class and attribute to that button, sets the text of the button, then finally appends the button to the buttonDisplay area.
 	function createButtons() {
@@ -60,43 +60,71 @@ $(document).ready(function() {
 			url: queryURL,
 			method: "GET"
 		}).then(function(response){
-			
+			console.log(response);
 			//Creating variable that gets us to the meat of the data.
 			var results = response.data;
 
 			//For loop that goes through our results and creates an image tag and paragraph tag for each response item.  
 			for (var i = 0; i < results.length; i++){
-				var gifImage = $("<img>");
 
 				//The paragraph tag's text is set to the iteration's rating value.
 				var p = $("<p>").text("Rating: " + results[i].rating);
 
-				//Variable that holds current iteration's gif
-				var imageUrl = results[i].images.fixed_height.url;
+				//Creating variable shortcut for navigating the response object.
+				var imagePath = results[i].images;
 
-				//Setting the src attribute to be the previous variable's value.
-				gifImage.attr("src", imageUrl);
+				//Creating an image tag for each iteration, adding gif as the class and setting various attributes.
+				var gifImage = $("<img>").addClass("gif").attr({
+					
+					//Setting the source of the image to the still version of the gif.
+					"src": imagePath.original_still.url,
+					
+					//Telling JS what data-still looks like, also happens to be the starting point when we load the gifs.
+                    "data-still": imagePath.original_still.url,
+					
+					//Telling JS what data-animated looks like.
+					"data-animate": imagePath.fixed_height.url,
 
-				//Adding an element of accesibility by setting the alt attribute to be the value of the user's click.
-				gifImage.attr("alt", userClick + " gif");
-			
+					//Telling JS we're starting out as still.
+					"data-state": "still",
+
+					//Adding an element of accessibility--our alt attribute.
+					"alt": userClick + " gif",
+
+					//Setting the height of the gif to 400 so it doesn't expand/contract when clicked on.
+                    "height": 400,
+                });
+
 				//Finally appending the gif and that gif's rating to the gifsContainer.
 				$("#gifsContainer").append(gifImage, p);
 			}
+			
+			//Click event for our loaded gifs.
+			$(".gif").on("click", function() {
 				
+				//variable that is grabbing the data-state of the clicked gif.
+				var state = $(this).attr("data-state");
+
+				//conditionals to route if the gif is still or animated and then changing the state based on which current state it's in to the opposite.
+				if (state === "still") {
+				  $(this).attr("src", $(this).attr("data-animate"));
+				  $(this).attr("data-state", "animate");
+				} else {
+				  $(this).attr("src", $(this).attr("data-still"));
+				  $(this).attr("data-state", "still");
+				}
+			  });
 		});
+
 	
 	});
 
 });
 
+//still picture path: results[i].images.original_still.url
+//gif in motion path: results[i].images.fixed_height.url
 
 
-
-
-
-
-//state--be able to start and stop the gif.  The gif loads in a still state
 
 //styling: 
 	//style the 9 gifs that are being displayed to the page currently.
